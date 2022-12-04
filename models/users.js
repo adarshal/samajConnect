d81e0b4924386fd591521e01361ee2457a646b53
adarshal <aalagade@gmail.com>
2022-12-05 03:29:41 +0530

    add confirm password input

--- models/users.js
+++ models/users.js
@@ -10,7 +10,7 @@ const UserSchema = new mongoose.Schema({
         type: String,
         required: true
             },
-    name:{
+     accountHolder:{
         type:String,
         required: true
     }
