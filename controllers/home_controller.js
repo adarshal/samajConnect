cde0d075ef5dccc93b4b4539f480b1a5742b7e8d
adarshal <aalagade@gmail.com>
2022-12-30 20:31:10 +0530

    remove bug causing wrong chat mail id going to backend

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,9 +3,11 @@ const User = require('../models/users');
 
 module.exports.home = async function (req, res) {
 
-    try {
         // populate the user of each post //25dec populate likes
-        let posts = await Post.find({})
+   
+        try{
+            // CHANGE :: populate the likes of each post and comment
+            let posts = await Post.find({})
             .sort('-createdAt')
             .populate('user')
             .populate({
@@ -13,21 +15,25 @@ module.exports.home = async function (req, res) {
                 populate: {
                     path: 'user'
                 },
-                populate:{
-                    path: 'likes' //likes of comment
+                populate: {
+                    path: 'likes'
                 }
             }).populate('likes');
-
-        let users = await User.find({});
-
-        return res.render('home', {
-            title: "Codeial | Home",
-            posts: posts,
-            user_list: users
-        });
-
-    } catch (err) {
-        console.log('error in showing home page', err)
+    
+        
+            let users = await User.find({});
+    
+            return res.render('home', {
+                title: "Codeial | Home",
+                posts:  posts,
+                all_users: users
+            });
+    
+        }catch(err){
+            console.log('Error', err);
+            return;
+        }
+       
     }
 
 
@@ -56,4 +62,4 @@ module.exports.home = async function (req, res) {
     //    return res.render('home',{
     //         title : 'Home'
     //     })
-}
+// }
