6df231ee482ab769887b957fa2ad809cac94ef71
Adarsh 2.O <aalagade@gmail.com>
2023-01-03 15:40:31 +0000

    Update chat_engine.js

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -3,7 +3,7 @@ class ChatEngine{
         this.chatBox = $(`#${chatBoxId}`);
         this.userEmail = userEmail;
 
-        this.socket = io.connect('http://localhost:5000');
+        this.socket = io.connect('http://3.112.234.159:8001');
 
         if (this.userEmail){
             this.connectionHandler();
